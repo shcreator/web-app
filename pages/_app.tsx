cf5f4c72efee033b8cc9aa547cb91466fdbc784e
shcreator <mainid24@outlook.com>
2024-10-07 21:54:06 +0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,9 +12,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <meta name="description" content="Your BIN Status 6 or 16 Digit BIN You can search using 6 digit BIN and get basic information about the bin." />
+        <meta name="keywords" content="bin checker cc, bin checker, bin checker free, credit card bin checker, cc bin checker, free bin checker, bin checker io, card bin checker, best bin checker, bin checker online." />
+        <title>Bin Checker</title>
 
         <link rel="manifest" href="/manifest.json" />
         <link
