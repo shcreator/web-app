5cc26c5a86f6171e3d4db0e9c5c88b789ed48a1c
shcreator <mainid24@outlook.com>
2024-10-07 22:16:15 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
+import styles from "/styles/Home.module.css";
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from "../styles/Home.module.css";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [bin, setBin] = useState('');
